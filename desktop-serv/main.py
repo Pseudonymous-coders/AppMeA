69f4e2b28b808ed3edb93f85c84c03f5294b01a4
Oliver Warne <OliverWarne@users.noreply.github.com>
2016-04-17 08:01:10 -0500

    data

--- desktop-serv/main.py
+++ desktop-serv/main.py
@@ -15,7 +15,22 @@ def main_loop():
     while True:
         graph.make_graph()
         link = upload("foo.png")
-        data.push(formatter(link,'test'),"and")
+        sd = run().get_counts()
+        to_email = ""
+        to_email += "Motion restlessness:"
+        to_email += str(sd[0]/2)
+        to_email += "\n"
+        to_email += "Accelerometer restlessness:"
+        to_email += str(sd[1])
+        to_email += "\n"
+        to_email += "Micrphone restlessness:"
+        to_email += str(sd[2])
+        to_email += "\n"
+
+        data.push(to_email,'data')
+
+        to_say = "Last night, you experienced " + str(sd[0]/2) + " counts of restlessness based on motion tracking"
+        data.push(formatter(link,to_say), "and")
         sleep(20)
 
 if __name__ == "__main__":
