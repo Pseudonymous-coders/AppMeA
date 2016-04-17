d17d13d47a4a1f9bf7c205c55b0e43b31022cd97
Eli Smith <plunkinguitar@gmail.com>
2016-04-17 04:40:51 -0500

    Create getDui.py

---
+++ UDOO/getDui.py
@@ -0,0 +1,18 @@
+#! /usr/bin/env python
+def doMic():
+        import serial
+        import time
+        global killed
+        from m2x.client import M2XClient
+        CLIENT = M2XClient(key="a3e04dc4b17ecb6574b7ae8c9198b3af")
+        DEVICE = CLIENT.device("72d48c4b1b7c8999cd5b4128a1337ae4")
+        STREAM = DEVICE.stream("mic")
+        ser = serial.Serial('/dev/ttyMCC')
+        while 1:
+                data = ser.readline()
+                data.strip("\n")
+                data.strip("")
+                data.strip(None)
+                STREAM.add_value(int(data))i
+                if killed:
+                        break
